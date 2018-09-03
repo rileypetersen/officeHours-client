434a02fbb8d3e3e3ba40565d5396b12a2148c851
Justin <justhays124@gmail.com>
2018-09-02 21:54:41 -0700

    prep for build with redux and router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+
 export class App extends Component {
 
   render() {
-    return (<div>
-      <BrowserRouter>
-        <div>
-          <NavComponent/>
-          <Route exact="exact" path="/" component={  }/>
-          <Footer/>
-        </div>
-      </BrowserRouter>
-    </div>);
+    return (
+      <div>
+        <BrowserRouter>
+          <div>
+
+            <Route exact="exact" path="/" component={  }/>
+ 
+          </div>
+        </BrowserRouter>
+      </div>
+    );
   }
+
 }
+
 
 export default App
