a9f3ab45c8b50da06f0b7cf13693604494c22113
Justin <justhays124@gmail.com>
2018-09-05 16:00:27 -0700

    Home component that will house users dashboard

---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+
+import { Dimmer, Loader, Transition } from 'semantic-ui-react';
+
+import NavBar from './DashBoard/NavBar';
+import Footer from './DashBoard/Footer';
+
+
+
+export class Home extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        // isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    // mocks api call...
+    // setTimeout(() => this.setState({ isLoading: false }), 500);
+  }
+
+  render() {
+    return (
+        <div className="home-desktop">
+          <NavBar />
+          <Footer />
+        </div>
+    );
+  };
+
+};
+
+
+export default Home;
