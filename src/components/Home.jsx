6f2124f9a7846edcec0fd56de4bb0a6ffb95bf70
Justin <justhays124@gmail.com>
2018-09-05 17:39:59 -0700

    formatting

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,24 +11,24 @@ import Footer from './DashBoard/Footer';
 export class Home extends Component {
   constructor(props) {
     super(props)
-      this.state = {
-        // isLoading: true
-    }
-  }
+		this.state = {
+			// isLoading: true
+		}
+	}
 
-  componentDidMount() {
-    // mocks api call...
-    // setTimeout(() => this.setState({ isLoading: false }), 500);
-  }
+	componentDidMount() {
+		// mocks api call...
+		// setTimeout(() => this.setState({ isLoading: false }), 500);
+	}
 
-  render() {
-    return (
-        <div className="home-desktop">
-          <NavBar />
-          <Footer />
-        </div>
-    );
-  };
+	render() {
+		return (
+			<div className="home-desktop">
+			<NavBar />
+			<Footer />
+			</div>
+		);
+	};
 
 };
 
