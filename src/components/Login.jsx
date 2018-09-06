5a1b2aa3e9602f34f18978c747c5726b75e2efa0
Justin <justhays124@gmail.com>
2018-09-05 17:34:24 -0700

    formatting

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,77 +7,73 @@ import './Login.css';
 
 
 class Login extends Component {
-  state = {
-    user_name: '',
-    password: '',
-    visible: true
-  };
+	state = {
+		user_name: '',
+		password: ''
+	};
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+	handleChange = event => {
+		this.setState({ [event.target.name]: event.target.value });
+	};
   
-  handleLogin = event => {
-    event.preventDefault();
-    console.log(this.state)
-    // this.props.userLogin(this.state, this.props.history);
-  };
+	handleLogin = event => {
+		event.preventDefault();
+		// this.props.userLogin(this.state, this.props.history);
+	};
 
-  render () {
-    return (
-    
-      <div className='login-form-desktop'>
-        <Grid textAlign='center' verticalAlign='middle'>
-          <Grid.Column>
-            <Header as='h2' textAlign='center'>
-              <Image src='/oh.jpg' /> Login to your account
-            </Header>
-            <Form size='large' onSubmit={ this.handleLogin }>
-              <Segment raised>
-                <Form.Input
-                  fluid
-                  required
-                  label='User Name'
-                  name='user_name'
-                  icon='user' 
-                  iconPosition='left' 
-                  placeholder='user.name'
-                  onChange={ (e) => this.handleChange(e) }
-                />
-                <Form.Input
-                  fluid
-                  required
-                  label='Password'
-                  name='password'
-                  icon='lock'
-                  iconPosition='left'
-                  placeholder='*******'
-                  type='password'
-                  onChange={ (e) => this.handleChange(e) }                  
-                />
-                <Button animated='fade' color='purple' fluid size='large'>
-                  <Button.Content visible>Login</Button.Content>
-                  <Button.Content hidden>LOGIN</Button.Content>
-                </Button>
-              </Segment>
-            </Form>
-            <Message>
-              New to Office Hours? <a href='/register'>Register</a>
-            </Message>
-          </Grid.Column>
-        </Grid>
-      </div>
-    
-    );
-  };
+	render () {
+		return (
+			<div className='login-form-desktop'>
+				<Grid textAlign='center' verticalAlign='middle'>
+					<Grid.Column>
+						<Header as='h2' textAlign='center'>
+							<Image src='/oh.jpg' /> Login to your account
+						</Header>
+						<Form size='large' onSubmit={ this.handleLogin }>
+						<Segment raised>
+							<Form.Input
+								fluid
+								required
+								label='User Name'
+								name='user_name'
+								icon='user' 
+								iconPosition='left' 
+								placeholder='user.name'
+								onChange={ (e) => this.handleChange(e) }
+							/>
+							<Form.Input
+								fluid
+								required
+								label='Password'
+								name='password'
+								icon='lock'
+								iconPosition='left'
+								placeholder='*******'
+								type='password'
+								onChange={ (e) => this.handleChange(e) }                  
+							/>
+							<Button animated='fade' color='purple' fluid size='large'>
+								<Button.Content visible>Login</Button.Content>
+								<Button.Content hidden>LOGIN</Button.Content>
+							</Button>
+						</Segment>
+						</Form>
+						<Message>
+							New to Office Hours? <a href='/register'>Register</a>
+						</Message>
+					</Grid.Column>
+				</Grid>
+			</div>
+		);
+	};
 };
 
 const mapStateToProps = state => ({
-  // showLoginError: state.auth.showLoginError
+  	// showLoginError: state.auth.showLoginError
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  // userLogin
+  	// userLogin
 }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
