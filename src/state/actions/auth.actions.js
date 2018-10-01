c217aeaec4a9a560d84311b47c5c27b90ea8db15
Justin <justhays124@gmail.com>
2018-10-01 12:08:12 -0700

    updating formatting and resonding info

--- src/state/actions/auth.actions.js
+++ src/state/actions/auth.actions.js
@@ -10,45 +10,45 @@ export const NOT_LOGGED_IN = 'NOT_LOGGED_IN';
 export const USER_LOGOUT = 'USER_LOGOUT';
 
 export const userLogin = (body, history) => {
-  return async (dispatch) => {
-    try {
-      dispatch({ type: USER_LOGIN_PENDING });
-      const payload = await authModel.default.userLogin(body, history);
-      dispatch({ type: USER_LOGIN_SUCCESS, payload });
-    } catch (err) {
-      dispatch({ type: USER_LOGIN_FAILED, payload: err });
-    }
-  }
+	return async (dispatch) => {
+		try {;
+			dispatch({ type: USER_LOGIN_PENDING });
+			const response = await authModel.default.userLogin(body, history);
+			console.log('the pay!', response);
+			dispatch({ type: USER_LOGIN_SUCCESS, payload: response.data.data });
+		} catch (err) {
+			dispatch({ type: USER_LOGIN_FAILED, payload: err.response.data.message });
+		}
+	};
 };
 
 export const userRegister = (newUser, history) => {
-  return async (dispatch) => {
-    try {
-      dispatch({ type: USER_REGISTER_PENDING });
-      const payload = await authModel.default.userRegister(newUser, history)
-      dispatch({ type: USER_REGISTER_SUCCESS, payload });
-    } catch (err) {
-      console.log('meee?',err.response)
-      dispatch({ type: USER_REGISTER_FAILED, payload: err.response.data.message })
-    }
-  }
+	return async (dispatch) => {
+		try {
+			dispatch({ type: USER_REGISTER_PENDING });
+			const payload = await authModel.default.userRegister(newUser, history);
+			dispatch({ type: USER_REGISTER_SUCCESS, payload });
+		} catch (err) {
+			dispatch({ type: USER_REGISTER_FAILED, payload: err.response.data.message });
+		}
+	};
 };
 
 export const getUser = () => {
-  return async (dispatch) => {
-    try {
-      const token = await authModel.getUser()
-      dispatch({ type: GET_USER, payload: token });
-    }
-    catch (err) {
-      dispatch({ type: NOT_LOGGED_IN, payload: err });
-    }
-  }
+	return async (dispatch) => {
+		try {
+			const token = await authModel.getUser();
+			dispatch({ type: GET_USER, payload: token });
+		}
+		catch (err) {
+			dispatch({ type: NOT_LOGGED_IN, payload: err });
+		}
+	};
 };
 
 export const userLogout = () => {
-  return (dispatch) => {
-    localStorage.removeItem('token');
-    dispatch({ type: USER_LOGOUT });
-  }
+  	return (dispatch) => {
+    	localStorage.removeItem('token');
+    	dispatch({ type: USER_LOGOUT });
+  	};
 };
--- src/state/models/auth.models.js
+++ src/state/models/auth.models.js
@@ -1,41 +1,27 @@
 import request from '../../helpers/request';
 
 const _authenticatedRequest = async () => {
-    const authToken = request('/auth/token')
-    return authToken.data
-}
+    const authToken = request('/auth/token');
+    return authToken.data;
+};
 
 const userLogin = async (body) => {
-    try {
-    // const [ scheme, token ] = userLogin.headers.authorization.split(' ')
-    // console.log("I should be a token!?!?! ", token)
-    // // let token = await request('/users/token')
-    // return token.data
-        const response = await request(`/users/login`, 'post', body);
-        const [ scheme, token ] = response.headers.auth.split(' ');
-        localStorage.setItem('officeHoursToken', token);
-        return response;
-    } catch(error) {
-        console.error(error)
-    }
-}
+    const response = await request(`/users/login`, 'post', body);
+    const [ scheme, token ] = response.headers.auth.split(' ');
+    localStorage.setItem('officeHoursToken', token);
+    return response;
+};
 
-const userRegister = async (newUser) => {
-    // try {
-        delete newUser.password2
-        const createdUser = await request('/users/register', 'post', newUser)
-        
-        console.log('fire reg?!!!!!!!!!!!!! ', createdUser)
-        // return createdUser.data
-    // } catch (error) {
-        // console.error('I fucked up? ',error)
-    // }
-}
+const userRegister = async (newUser) => {;
+    delete newUser.password2;
+    const createdUser = await request('/users/register', 'post', newUser);
+    return createdUser.data.data;
+};
 
 const getUser = async () => {
-    const token = await request(`/users/token`)
-    return token.data
-}
+    const token = await request(`/users/token`);
+    return token.data;
+};
 
 
-export default { _authenticatedRequest, userLogin, userRegister, getUser }
+export default { _authenticatedRequest, userLogin, userRegister, getUser };
