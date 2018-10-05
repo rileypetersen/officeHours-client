13ba68e3d680e18be0d4aa27d0bf5cb0616ff61b
Justin <justhays124@gmail.com>
2018-10-05 15:35:23 -0700

    altering orgList styling

--- src/components/OrgList/OrgList.jsx
+++ src/components/OrgList/OrgList.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { List } from 'semantic-ui-react';
+import { List, Grid, Form, Segment } from 'semantic-ui-react';
 import { orgsActions } from '../../state/actions';
 import OrgListItem from './OrgListItem'
 const { getAllOrgs } = orgsActions;
@@ -13,21 +13,29 @@ class OrgList extends Component {
     }
 
     componentDidMount = async () => {
-		await this.props.getAllOrgs()
+        await this.props.getAllOrgs()
     }
 
     render() {
 		
         return (
-			<List divided relaxed>
-				{ this.props.orgs.map((org, i) => < OrgListItem key={i} org={org} />) }
-			</List>
+        <Grid textAlign='center' verticalAlign='middle'>
+            <Grid.Column>
+                <Form size='large' onSubmit={ this.handleLogin }>
+                    <Segment raised>
+                        <List divided relaxed>
+                            { this.props.orgs.map((org, i) => < OrgListItem key={i} org={org} />) }
+                        </List>
+                    </Segment>
+                </Form>
+            </Grid.Column>
+        </Grid>
         )
     }
 };
 
 const mapStateToProps = (state) => {
-  	return { showGetAllOrgsError: state.orgsReducers.showGetAllOrgsError, orgs: state.orgsReducers.orgs }
+  	return { showGetAllOrgsError: state.orgsReducers.showGetAllOrgsError, orgs: state.orgsReducers.orgs, usersOrgs: state.authReducers.user.organizations }
 };
 
 const mapDispatchToProps = (dispatch) => bindActionCreators({ getAllOrgs }, dispatch);
