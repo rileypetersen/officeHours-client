15a1d24d9d6ab1bf4980c522791173db67306c87
Curtis <curtis.cawley@icloud.com>
2018-10-05 13:00:57 -0700

    created reducers for organizations

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,13 +1,15 @@
 // import all reducers then...
 import authReducers from './auth.reducers';
 import usersReducers from './users.reducers';
+import orgsReducers from './organizations.reducers';
 
 import { combineReducers } from 'redux';
 
 const rootReducer = combineReducers({
     // combine them
     authReducers,
-    usersReducers
+    usersReducers,
+    orgsReducers
 });
 
 export default rootReducer;
