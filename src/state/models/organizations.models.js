dc16c6d40b69f24a95c691ee0533daf6e44c1767
Curtis <curtis.cawley@icloud.com>
2018-10-05 12:59:30 -0700

    created request to get all orgs from api

--- src/state/models/organizations.models.js
+++ src/state/models/organizations.models.js
@@ -1,9 +1,7 @@
 import request from '../../helpers/request';
 
 const getAllOrgs = async () => {
-    console.log('fuckinghello');
     const orgs = await request('/organizations');
-    console.log(orgs.data);
     return orgs.data.data;
 };
 
