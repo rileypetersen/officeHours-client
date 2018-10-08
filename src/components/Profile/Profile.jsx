9f37b382286371d6813aa54b95bad5ec8d32803d
Justin <justhays124@gmail.com>
2018-10-08 12:38:05 -0700

    shell for profile component

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {  } from 'semantic-ui-react';
+import { usersActions } from '../../state/actions';
+const {  } = usersActions;
+
+class Profile extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount = async () => {
+    }
+
+    render() {
+        return (
+        <div>
+
+        </div>
+        )
+    }
+};
+
+const mapStateToProps = (state) => {
+  	return {  }
+};
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({  }, dispatch);
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Profile));
