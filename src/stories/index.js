9189640a0708cbc6cbaecce8f8d0f6a62d5c2312
Riley Petersen <12@rjp.me>
2018-10-11 11:45:37 -0700

    added stories and installed reactsemantic-ui-calendar-react to serve as datetime picker

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,8 +2,23 @@
 import React from 'react';
 
 //Semantic 
-import { Button, Card, Icon, Image, Label, Tab } from 'semantic-ui-react';
+
 import 'semantic-ui-css/semantic.min.css'
+import { 
+  Button, 
+  Card, 
+  Icon, 
+  Image, 
+  Label, 
+  Tab, 
+  Form,
+  Input,
+  Container,
+  select,
+  Dropdown
+} 
+from 'semantic-ui-react';
+
 
 //Stories
 import { storiesOf } from '@storybook/react';
@@ -16,8 +31,19 @@ import { setOptions } from '@storybook/addon-options'
 
 
 
+//                //
+//                //
+//     Stories    //
+//                //
+//                //
+
+
+
+
+ //Button
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () => 
+    <Button onClick={action('clicked')}>Hello Button</Button>)
   .add('with some emoji', () => (
     <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
@@ -48,9 +74,10 @@ storiesOf('Card', module)
   </Card>
 ))
 
-storiesOf('orgprofile', module)
-.add('example org segment', () => (
+storiesOf('Profile', module)
+.add('profile_card', () => (
   // Group Profile "card" 
+
     <Card>
       <Card.Content>
         <Image floated='left' size='tiny' src='https://react.semantic-ui.com/images/avatar/large/steve.jpg' />
@@ -104,4 +131,84 @@ storiesOf('tabs', module)
 // .add('tabs', () => (
   .add('TabExampleSecondaryPointing', () => (
     <Tab menu={{ secondary: true, pointing: true }} panes={panes} />))
-  
+
+
+//Forms
+
+storiesOf('createProfile', module)
+.add('createProfile', () =>(
+  <Container>
+    <Form>
+        <Form.Group widths='equal'>
+          <Form.Input fluid label='First name' placeholder='First name' />
+          <Form.Input fluid label='Last name' placeholder='Last name' />
+        </Form.Group>
+        <Form.Group widths='equal'>
+          <Form.Input label='Title' placeholder='Product Designer at Amazon' />
+          <Form.Input label='Linkedin' placeholder='http://www.linkedin.com/in/...' />
+        </Form.Group>  
+        <Form.TextArea label='Bio' placeholder='Tell us more about you...' />
+        <Form.Dropdown  label='Tags' placeholder='Tag' fluid multiple search selection options={[
+          {key: "kills", value: 'kills', text: "kills"},
+          {key: "Angular", value: 'Angular', text: "Angular"},
+          {key: "CSS", value: 'CSS', text: "CSS"},
+          {key: "Graphic", value: 'Graphic', text: "Graphic Design"},
+          {key: "Ember", value: 'Ember', text: "Ember"},
+          {key: "HTML", value: 'HTML', text: "HTML"},
+          {key: "Information", value: 'Information', text: "Information Architecture"},
+          {key: "Javascript", value: 'Javascript', text: "Javascript"},
+          {key: "Mechanical", value: 'Mechanical', text: "Mechanical Engineering"},
+          {key: "Meteor", value: 'Meteor', text: "Meteor"},
+          {key: "NodeJS", value: 'NodeJS', text: "NodeJS"},
+          {key: "Plumbing", value: 'Plumbing', text: "Plumbing"},
+          {key: "Python", value: 'Python', text: "Python"},
+          {key: "Rails", value: 'Rails', text: "Rails"},
+          {key: "React", value: 'React', text: "React"},
+          {key: "Kitchen", value: 'Kitchen', text: "Kitchen Repair"},
+          {key: "Ruby", value: 'Ruby', text: "Ruby"},
+          {key: "UI", value: 'UI', text: "UI Design"},
+          {key: "User", value: 'User', text: "User Experience"}]}>
+        </Form.Dropdown>
+      <Button primary floated='right'>Submit</Button>
+    </Form>
+   </Container>
+))
+
+storiesOf('createSession', module)
+.add('createSession', () =>(
+  <Container>
+    <Form>
+        <Form.Group widths='equal'>
+          <Form.Input fluid label='First name' placeholder='First name' />
+          <Form.Input fluid label='Last name' placeholder='Last name' />
+        </Form.Group>
+        <Form.Group widths='equal'>
+          <Form.Input label='Title' placeholder='Product Designer at Amazon' />
+          <Form.Input label='Linkedin' placeholder='http://www.linkedin.com/in/...' />
+        </Form.Group>  
+        <Form.TextArea label='Bio' placeholder='Tell us more about you...' />
+        <Form.Dropdown  label='Tags' placeholder='Tag' fluid multiple search selection options={[
+          {key: "kills", value: 'kills', text: "kills"},
+          {key: "Angular", value: 'Angular', text: "Angular"},
+          {key: "CSS", value: 'CSS', text: "CSS"},
+          {key: "Graphic", value: 'Graphic', text: "Graphic Design"},
+          {key: "Ember", value: 'Ember', text: "Ember"},
+          {key: "HTML", value: 'HTML', text: "HTML"},
+          {key: "Information", value: 'Information', text: "Information Architecture"},
+          {key: "Javascript", value: 'Javascript', text: "Javascript"},
+          {key: "Mechanical", value: 'Mechanical', text: "Mechanical Engineering"},
+          {key: "Meteor", value: 'Meteor', text: "Meteor"},
+          {key: "NodeJS", value: 'NodeJS', text: "NodeJS"},
+          {key: "Plumbing", value: 'Plumbing', text: "Plumbing"},
+          {key: "Python", value: 'Python', text: "Python"},
+          {key: "Rails", value: 'Rails', text: "Rails"},
+          {key: "React", value: 'React', text: "React"},
+          {key: "Kitchen", value: 'Kitchen', text: "Kitchen Repair"},
+          {key: "Ruby", value: 'Ruby', text: "Ruby"},
+          {key: "UI", value: 'UI', text: "UI Design"},
+          {key: "User", value: 'User', text: "User Experience"}]}>
+        </Form.Dropdown>
+      <Button primary floated='right'>Submit</Button>
+    </Form>
+   </Container>
+))
