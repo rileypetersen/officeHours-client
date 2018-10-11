c0ecf748b343a91c27a789b681a7d970abea12dc
Justin <justhays124@gmail.com>
2018-10-11 12:55:39 -0700

    framing for current/logged in user focused reducers

--- src/state/reducers/users.reducers.js
+++ src/state/reducers/users.reducers.js
@@ -1,8 +1,24 @@
 import { usersActions } from '../actions'
 
 const {
-	  GET_ALL_USERS,
-	  GET_ONE_USER
+		GET_ALL_USERS_PENDING,
+		GET_ALL_USERS_SUCCESS,
+		GET_ALL_USERS_FAILED,
+		GET_ONE_USER_PENDING,
+		GET_ONE_USER_SUCCESS,
+		GET_ONE_USER_FAILED,
+		GET_USER_TAGS_PENDING,
+		GET_USER_TAGS_SUCCESS,
+		GET_USER_TAGS_FAILED,
+		ADD_OR_REMOVE_USER_TAG_PENDING,
+		ADD_OR_REMOVE_USER_TAG_SUCCESS,
+		ADD_OR_REMOVE_USER_TAG_FAILED,
+		UPDATE_USER_PENDING,
+		UPDATE_USER_SUCCESS,
+		UPDATE_USER_FAILED,
+		DELETE_USER_PENDING,
+		DELETE_USER_SUCCESS,
+		DELETE_USER_FAILED
 } = usersActions
 
 let INITIAL_VALUE = {
@@ -11,10 +27,43 @@ let INITIAL_VALUE = {
 
 export default (state = INITIAL_VALUE, action) => {
   switch (action.type) {
-    case GET_ALL_USERS:
-		return action.payload
-	case GET_ONE_USER:
-      	return action.payload
+	case GET_ALL_USERS_PENDING:
+		return { ...state, isLoading: true }
+	case GET_ALL_USERS_SUCCESS:
+		return { ...state, isLoading: false }
+	case GET_ALL_USERS_FAILED:
+		return { ...state, isLoading: false }
+	case GET_ONE_USER_PENDING:
+		return { ...state, isLoading: true }
+	case GET_ONE_USER_SUCCESS:
+		return { ...state, isLoading: false }
+	case GET_ONE_USER_FAILED:
+		return { ...state, isLoading: false }
+	case GET_USER_TAGS_PENDING:
+		return { ...state, isLoading: true }
+	case GET_USER_TAGS_SUCCESS:
+		return { ...state, isLoading: false }
+	case GET_USER_TAGS_FAILED:
+		return { ...state, isLoading: false }
+	case ADD_OR_REMOVE_USER_TAG_PENDING:
+		return { ...state, isLoading: true }
+	case ADD_OR_REMOVE_USER_TAG_SUCCESS:
+		return { ...state, isLoading: false }
+	case ADD_OR_REMOVE_USER_TAG_FAILED:
+		return { ...state, isLoading: false }
+	case UPDATE_USER_PENDING:
+		return { ...state, isLoading: true }
+	case UPDATE_USER_SUCCESS:
+		return { ...state, isLoading: false }
+	case UPDATE_USER_FAILED:
+		return { ...state, isLoading: false }
+	case DELETE_USER_PENDING:
+		return { ...state, isLoading: true }
+	case DELETE_USER_SUCCESS:
+		return { ...state, isLoading: false }
+	case DELETE_USER_FAILED:
+		return { ...state, isLoading: false }
+
     default:
       	return state
   }
