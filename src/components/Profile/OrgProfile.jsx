70052cd110fe0c9e8233feec8e9472474ffa5206
Justin <justhays124@gmail.com>
2018-10-12 16:46:30 -0700

    added rough mapping for info from store

--- src/components/Profile/OrgProfile.jsx
+++ src/components/Profile/OrgProfile.jsx
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { Card, Button, Image, Label } from 'semantic-ui-react';
+import Tabs from '../Tabs/Tabs'
 import { orgsActions } from '../../state/actions';
 const {  } = orgsActions;
 
@@ -15,16 +16,25 @@ class OrgProfile extends Component {
     }
 
     render() {
+        // id: 1, organizer_id: 1, name: "WTIA", short_description: "Washington Technology Industry Association is a tr… of the technology community in Washington state.", long_description: "WTIA is an influential co-op of 800 tech companies…n and government leaders building a better world.", …}
+        //     hosts_can_create_sessions true
+        //     id 1
+        //     logo_img_url "http /i1.wp.com/www.dkparker.com/wp-content/uploads/2018/04/wtia-startupclub-logo-800px.png?fit=800%2C274"
+        //     long_description "WTIA is an influential co-op of 800 tech companies large and small. We are pragmatic partners with education and government leaders building a better world."
+        //     name "WTIA"
+        //     organizer_id 1
+        //     short_description "Washington Technology Industry Association is a trade association unifying the voice of the technology community in Washington state."
+        //     users (7) [{…}, {…}, {…}, {…}, {…}, {…}, {…}]
+        //     website_url "http://www.washingtontechnology.org"
         console.log(this.props.match.params)
         console.log('the org!', this.props.currentOrg)
         return (
         <div>
-            <p>I'M AN ORG!</p>
             <Card>
                 <Card.Content>
-                    <Image floated='left' size='tiny' src='https://react.semantic-ui.com/images/avatar/large/steve.jpg' />
-                    <Card.Header>Steve Sanders</Card.Header>
-                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Image floated='left' size='tiny' src={this.props.currentOrg.logo_img_url} />
+                    <Card.Header>{this.props.currentOrg.name}</Card.Header>
+                    <Card.Meta>{this.props.currentOrg.short_description}</Card.Meta>
                     <Label size='mini' padded>Product</Label>
                     <Label size='mini' padded>Design</Label>
                     <Label size='mini' padded>Software</Label>
@@ -39,6 +49,7 @@ class OrgProfile extends Component {
                     <Button color="green" size="mini" compact>12:00 AM</Button>
                 </Card.Content>
             </Card>
+            { <Tabs orgMembers={this.props.currentOrg.users} /> }
         </div>
         )
     }
--- src/components/Profile/UserProfile.jsx
+++ src/components/Profile/UserProfile.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import {  } from 'semantic-ui-react';
+import { Card, Grid, Image, Button, Label } from 'semantic-ui-react';
 import { usersActions } from '../../state/actions';
 const {  } = usersActions;
 
@@ -15,11 +15,29 @@ class UserProfile extends Component {
     }
 
     render() {
-        console.log(this.props.match.params)
+        console.log(this.props.member)
         return (
-        <div>
-            <p>I'M A USER!</p>
-        </div>
+            <Grid.Column>
+            <Card>
+                <Card.Content>
+                    <Image floated='left' size='tiny' src='https://react.semantic-ui.com/images/avatar/large/steve.jpg' />
+                    <Card.Header>{this.props.member.first_name} {this.props.member.last_name}</Card.Header>
+                    <Card.Meta>Friends of Elliot</Card.Meta>
+                    <Label size='mini' padded>Product</Label>
+                    <Label size='mini' padded>Design</Label>
+                    <Label size='mini' padded>Software</Label>
+                    <Card.Description>
+                        Steve wants to add you to the group <strong>best friends</strong>
+                    </Card.Description>
+                </Card.Content>
+                <Card.Content extra>
+                    <Button color="green" size="mini" compact>12:00 AM</Button>
+                    <Button color="green" size="mini" compact>12:00 AM</Button>
+                    <Button color="green" size="mini" compact>12:00 AM</Button>
+                    <Button color="green" size="mini" compact>12:00 AM</Button>
+                </Card.Content>
+            </Card>
+        </Grid.Column>
         )
     }
 };
