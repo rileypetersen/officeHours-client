648f020e6a2e87f402f6e249515e8ad0ea0d4c15
Justin <justhays124@gmail.com>
2018-10-15 00:00:49 -0700

    add/remove org user dynamic

--- src/components/OrgList/OrgListItem.jsx
+++ src/components/OrgList/OrgListItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Item, Button } from 'semantic-ui-react';
 
-const OrgListItem = ({ org, getOneOrg, history, addOrgUser, userId }) => {
+const OrgListItem = ({ org, getOneOrg, history, addOrgUser, removeOrgUser, userId }) => {
     return (
         <Item onClick={ () => getOneOrg(org.id, org.name, history) } >
             <Item.Image size='small' src={ org.logo_img_url } />
@@ -15,7 +15,15 @@ const OrgListItem = ({ org, getOneOrg, history, addOrgUser, userId }) => {
             </Item.Content>
             <Item.Content>
                 <Item.Extra>
-                    <Button onClick={ () => addOrgUser(org.id, { user_id: userId, organization_id: org.id }) } floated='right'>{ org.joined ? 'Leave' : 'Join' }</Button>
+                    { org.joined ? 
+                        <Button onClick={ () => addOrgUser(org.id, { user_id: userId, organization_id: org.id }) } floated='right'>
+                            Join
+                        </Button>
+                    :
+                        <Button onClick={ () => removeOrgUser(org.id, { user_id: userId, organization_id: org.id }) } floated='right'>
+                            Leave
+                        </Button>
+                    }
                 </Item.Extra>
             </Item.Content>
         </Item>
