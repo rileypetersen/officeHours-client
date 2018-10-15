b0de779e2e9516dff5862a3560cd8650d3ce21c4
Justin <justhays124@gmail.com>
2018-10-15 16:07:45 -0700

    corrected tab formatting issue

--- src/state/reducers/auth.reducers.js
+++ src/state/reducers/auth.reducers.js
@@ -23,33 +23,33 @@ let INITIAL_STATE = {
 }
 
 export default (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case USER_LOGIN_PENDING:
-      	return { ...state, isLoading: true }
-    case USER_LOGIN_SUCCESS:
-      	return { ...state, isLoading: false, isLoggedIn: true, showLoginError: false, user: action.payload }
-    case USER_LOGIN_FAILED:
-		return { ...state, showLoginError: true }
-	case USER_LOGOUT_PENDING:
-		return { ...state, isLoading: true }
-	case USER_LOGOUT_SUCCESS:
-		return { isLoading: false, isLoggedIn: false, showLoginError: false, showRegisterError: false, user: {} }
-	case USER_LOGOUT_FAILED:
-		return { ...state, isLoading: false }
-    case USER_REGISTER_PENDING:   
-        return { ...state, isLoading: true }
-    case USER_REGISTER_SUCCESS: 
-      	return { ...state, showRegisterError: false, showRegisterSuccess: true }
-    case USER_REGISTER_FAILED: 
-		return { ...state, showRegisterError: true, showRegisterSuccess: false, registerErrorMessage: action.payload }
-	case GET_USER_VIA_TOKEN:
-		return { ...state, }
-	case NOT_LOGGED_IN:
-		return { ...state, isLoading: false, isLoggedIn: false, showLoginError: false, showRegisterError: false, user: {} }
+	switch (action.type) {
+		case USER_LOGIN_PENDING:
+			return { ...state, isLoading: true }
+		case USER_LOGIN_SUCCESS:
+			return { ...state, isLoading: false, isLoggedIn: true, showLoginError: false, user: action.payload }
+		case USER_LOGIN_FAILED:
+			return { ...state, showLoginError: true }
+		case USER_LOGOUT_PENDING:
+			return { ...state, isLoading: true }
+		case USER_LOGOUT_SUCCESS:
+			return { isLoading: false, isLoggedIn: false, showLoginError: false, showRegisterError: false, user: {} }
+		case USER_LOGOUT_FAILED:
+			return { ...state, isLoading: false }
+		case USER_REGISTER_PENDING:   
+			return { ...state, isLoading: true }
+		case USER_REGISTER_SUCCESS: 
+			return { ...state, showRegisterError: false, showRegisterSuccess: true }
+		case USER_REGISTER_FAILED: 
+			return { ...state, showRegisterError: true, showRegisterSuccess: false, registerErrorMessage: action.payload }
+		case GET_USER_VIA_TOKEN:
+			return { ...state, }
+		case NOT_LOGGED_IN:
+			return { ...state, isLoading: false, isLoggedIn: false, showLoginError: false, showRegisterError: false, user: {} }
 
-    default:
-      	return state
-  }
+		default:
+			return state
+	}
 }
 
   
