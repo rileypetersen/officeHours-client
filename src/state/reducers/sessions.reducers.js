f0528ff5435775523456612c180871ada976a8f9
Justin <justhays124@gmail.com>
2018-10-15 17:16:04 -0700

    created sessions reducer and building out for session creation

---
+++ src/state/reducers/sessions.reducers.js
@@ -0,0 +1,28 @@
+import { sessionsActions } from '../actions';
+
+const {
+	CREATE_SESSION_PENDING,
+	CREATE_SESSION_SUCCESS,
+	CREATE_SESSION_FAILED
+} = authActions
+
+let INITIAL_STATE = {
+	isLoading: false,
+	showSessionError: false
+}
+
+export default (state = INITIAL_STATE, action) => {
+	switch (action.type) {
+		case CREATE_SESSION_PENDING:
+			return { ...state, isLoading: true }
+		case CREATE_SESSION_SUCCESS:
+			return { ...state, isLoading: false, isLoggedIn: true, showSessionError: false }
+		case CREATE_SESSION_FAILED:
+			return { ...state, showSessionError: true }
+		
+		default:
+			return state
+	}
+}
+
+  
