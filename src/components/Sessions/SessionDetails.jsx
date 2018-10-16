66843158edbd0a39720892bf18dd4b36bd004b5e
Riley Petersen <12@rjp.me>
2018-10-15 17:46:49 -0700

    no changes

--- src/components/Sessions/SessionDetails.jsx
+++ src/components/Sessions/SessionDetails.jsx
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Item, Header, Label, Button } from 'semantic-ui-react';
+import { Item, Header, Label, Button, Input } from 'semantic-ui-react';
 import { usersActions } from '../../state/actions';
 import moment from 'moment'
 
 const {  } = usersActions;
+
+const book_meeting = () => (
+    <Modal trigger={<Button>Show Modal</Button>} centered={false} closeIcon>
+      <Modal.Header>Book a meeting with { host }</Modal.Header>
+      <Modal.Content image>
+        <Image wrapped size='medium' src='/images/avatar/large/rachel.png' />
+        <Modal.Description>
+          <Header>Default Profile Image</Header>
+          <p>We've found the following gravatar image associated with your e-mail address.</p>
+          <p>Is it okay to use this photo?</p>
+        </Modal.Description>
+        <Input></Input>
+        <Input></Input>
+        <Input></Input>
+        <Button>Book</Button>
+
+      </Modal.Content>
+    </Modal>
+  )
 
 class SessionDetails extends Component {
     constructor(props) {
@@ -20,8 +39,7 @@ class SessionDetails extends Component {
     render() {
         console.log(this.props.session)
         let meetings = this.props.session.meetings || []
-        let { date, location } = this.props.session
-        
+        let { date, location } = this.props.session        
         let profile_img_url = this.props.session.host.profile_img_url || 'http://profilepicturesdp.com/wp-content/uploads/2018/06/default-profile-picture-circle-5.png'
         let first_name = this.props.session.host.first_name || 'HOST'
         let last_name = this.props.session.host.last_name || 'NEEDED' 
@@ -40,6 +58,7 @@ class SessionDetails extends Component {
               </Item.Content>
             </Item>
           </Item.Group>
+          
         )    }
 };
 
